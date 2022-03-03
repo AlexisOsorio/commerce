131b1ef82b3c40c2df1e2d707a6a21edc0d5ee11
Kevin Osorio <kevinosorio201@gmail.com>
2022-03-03 00:47:03 -0500

    arreglos y nueva pagina de login

--- views/layouts/footer.php
+++ views/layouts/footer.php
@@ -1,29 +1,31 @@
     <!-- /.content -->
     </div>
-  <!-- /.content-wrapper -->
+    <!-- /.content-wrapper -->
 
-  <footer class="main-footer">
-    <div class="float-right d-none d-sm-block">
-      <b>Version</b> 3.2.0
+    <footer class="main-footer">
+      <div class="float-right d-none d-sm-block">
+        <b>Version</b> 3.2.0
+      </div>
+      <strong>Copyright &copy; 2014-2021 <a href="https://adminlte.io">AdminLTE.io</a>.</strong> All rights reserved.
+    </footer>
+
+    <!-- Control Sidebar -->
+    <aside class="control-sidebar control-sidebar-dark">
+      <!-- Control sidebar content goes here -->
+    </aside>
+    <!-- /.control-sidebar -->
     </div>
-    <strong>Copyright &copy; 2014-2021 <a href="https://adminlte.io">AdminLTE.io</a>.</strong> All rights reserved.
-  </footer>
+    <!-- ./wrapper -->
 
-  <!-- Control Sidebar -->
-  <aside class="control-sidebar control-sidebar-dark">
-    <!-- Control sidebar content goes here -->
-  </aside>
-  <!-- /.control-sidebar -->
-</div>
-<!-- ./wrapper -->
+    <!-- jQuery -->
+    <script src="utils/js/jquery.min.js"></script>
+    <!-- Bootstrap 4 -->
+    <script src="utils/js/bootstrap.bundle.min.js"></script>
+    <!-- AdminLTE App -->
+    <script src="utils/js/adminlte.min.js"></script>
+    <!-- AdminLTE for demo purposes -->
+    <script src="utils/js/demo.js"></script>
+    <script src="utils/js/bootstrap.min.js"></script>
+    </body>
 
-<!-- jQuery -->
-<script src="utils/js/jquery.min.js"></script>
-<!-- Bootstrap 4 -->
-<script src="utils/js/bootstrap.bundle.min.js"></script>
-<!-- AdminLTE App -->
-<script src="utils/js/adminlte.min.js"></script>
-<!-- AdminLTE for demo purposes -->
-<script src="utils/js/demo.js"></script>
-</body>
-</html>
+    </html>
